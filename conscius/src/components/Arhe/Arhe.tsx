a6e252f98a3e5121e91733b8fbb3b9b76ce59eaa
R3VV1L <r3vv1l@mail.ru>
2023-07-22 00:17:30 +0300

    update Arhe

--- conscius/src/components/Arhe/Arhe.tsx
+++ conscius/src/components/Arhe/Arhe.tsx
@@ -12,33 +12,37 @@ export const Arhe = () => {
             .then((data) => setData(data));
     }, []);
 
+    const genre = data.flat(2).map((item) => item.genre);
+    const game = data.flat(2).map((item) => item.game);
+    //добавить филтрацию по жанру
     const myTreeData = [
         {
-            name: 'Parent Node',
+            name: 'Game 0',
             children: [
                 {
-                    name: 'Child Node',
+                    name: 'Game 1',
+                    children: [
+                        {
+                            name: 'Game 1.1 . . .',
+                        },
+                    ],
                 },
                 {
-                    name: 'Child Node',
+                    name: 'Game 2',
                 },
             ],
         },
     ];
-    console.log(data);
-    // console.log(
-    //     data
-    //         .flat(2)
-    //         .map((e) => e.genre)
-    //         .toString()
-    // );
+
+    console.log(genre, '/', game);
 
     return (
         <div>
+            {genre.toString()}
             <div id="treeWrapper" style={{ width: '100%', height: '90vh' }}>
                 <Tree
                     data={myTreeData}
-                    orientation="horizontal"
+                    orientation="vertical"
                     translate={{ x: 650, y: 350 }}
                     separation={{ siblings: 2, nonSiblings: 2 }}
                     nodeSize={{ x: 200, y: 100 }}
